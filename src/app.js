917198e571d833c362649ebee35f20fd7d6ffb92
FractureDVL <joseandrescram1005@gmail.com>
2022-05-18 21:45:34 -0500

    Config the project

---
+++ src/app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const path = require('path');
+//initializations
+
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', engine({
+    defaultLayout: 'main',
+    layoutDir: path.join(app.get('views'), 'layouts'),
+    partialDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars'),
+
+}));
+
+app.set('view engine', '.hbs');
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//Global variables
+app.use((req, res, next) => {
+
+    next();
+})
+
+//Routes
+app.use(require('./routes/index'));
+app.use(require('./routes/authentication'));
+app.use('/links', require('./routes/links'));
+
+//Public
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Starting the server
+
+app.listen(app.get("port"));
+console.log("Server is in port", app.get("port"));
