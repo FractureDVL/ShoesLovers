3bf19fc4ea10126d159007131f614d4d7b82cf62
FractureDVL <joseandrescram1005@gmail.com>
2022-05-19 00:26:34 -0500

    config Database

---
+++ database/db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE shoes_lovers;
+
+USE shoes_lovers;
+
+
---
+++ src/keys.js
@@ -0,0 +1,10 @@
+module.export = {
+    database: {
+        host: 'localhost',
+        user:'root',
+        password: '',
+        database:'shoes_lovers'
+    }
+
+
+}
