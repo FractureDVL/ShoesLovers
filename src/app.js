ef7ba03ca6a59137197d6644217dad135285af3a
FractureDVL <joseandrescram1005@gmail.com>
2022-05-22 01:34:28 -0500

    Create local var for session

--- src/app.js
+++ src/app.js
@@ -49,6 +49,7 @@ app.use(passport.session());
 app.use((req, res, next) => {
     app.locals.success = req.flash('success');
     app.locals.success = req.flash('message');
+    app.locals.user = req.user;
     next();
 })
 
