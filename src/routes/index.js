bf885f7b804ed5446052a9e2f30bf680839bdd27
iKennMarcucci <marcucci5783@gmail.com>
2022-06-12 22:35:17 -0500

    config fixed

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  res.render("links/index");
+  const zapato = await pool.query("SELECT * FROM zapatos WHERE serial = 1030 AND nombre != 'Slvrs' AND nombre != 'MoonStrickers'");
+  const slvrs = await pool.query("SELECT * FROM zapatos WHERE nombre = 'Slvrs'");
+  const moonstrickers = await pool.query("SELECT * FROM zapatos WHERE nombre = 'MoonStrickers'");
+  res.render("links/index", {zapato, slvrs, moonstrickers});
 });
 
 module.exports = router;
