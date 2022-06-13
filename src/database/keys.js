ec2ec64edf73f61e47e8b99d09256079dbce80b1
iKennMarcucci <marcucci5783@gmail.com>
2022-06-12 22:22:28 -0500

    Fixing deploy config

--- src/database/keys.js
+++ src/database/keys.js
@@ -1,9 +1,25 @@
+// PARA LOCAL ACTIVAR
+
+// module.exports = {
+//   database: {
+//     connectionLimit: 10,
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "shoes_lovers",
+//   },
+// };
+
+
+// PARA DEPLOY ACTIVAR
+const mysql = require('mysql')
+
 module.exports = {
-  database: {
-    connectionLimit: 10,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "shoes_lovers",
-  },
-};
+
+    database: {
+        host: 'us-cdbr-east-05.cleardb.net',
+        user: 'b22afc9f4a2146',
+        database: 'heroku_7d7ccff9ddac8b9',
+        password: 'b87d6787'
+    }
+}
